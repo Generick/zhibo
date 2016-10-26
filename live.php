9e7d16dee1c6537f06f05ac61da6a17b979a7769
940613683 <940613683@qq.com>
2016-10-26 17:12:26 +0800

    qqgame

--- live.php
+++ live.php
@@ -40,9 +40,9 @@ if($liveDT){
 $showinfo=$db->CacheGetRow(10,"select u.userId as userId,u.avatar as avatar,u.nickname as nickname,u.city as city,a.roomNumber as roomNumber from bu_user_anchors a LEFT JOIN bu_user u on a.userId = u.userId WHERE a.roomNumber = $roomnumber and a.`status` =1 and u.`status` =1");
 
 if(!$showinfo){
-	include($app_path."include/footer.inc.php");
-	header("Location:/blank.php");
-	exit;
+    include($app_path."include/footer.inc.php");
+    header("Location:/blank.php");
+    exit;
 }
 $showinfo=safe_output($showinfo);
 
@@ -60,10 +60,10 @@ if($roomnumber==$user['roomNumber']){
 }
 $vhistory=explode(',',$_COOKIE['vhistory']);
 if(!in_array($roomnumber,$vhistory)){
-	array_unshift($vhistory,$roomnumber);
-	$carr=array_chunk($vhistory,5);
-	$result=join(',',$carr[0]);
-	setcookie('vhistory',$result,time()+3600*356*10,'/',_COOKIE_DOMAIN_);
+    array_unshift($vhistory,$roomnumber);
+    $carr=array_chunk($vhistory,5);
+    $result=join(',',$carr[0]);
+    setcookie('vhistory',$result,time()+3600*356*10,'/',_COOKIE_DOMAIN_);
 }
 //礼物
 $rs=$db->Execute("select * from gift a, giftcate b where a.giftcateid =b.giftcateid AND b.type=0 order by a.indexs DESC");
@@ -72,21 +72,21 @@ $giftId = $giftinfo = array();
 $zhou_xing = explode(",",$global_config_data["top_week0"].",".$global_config_data["top_week1"]);
 $giftId = $giftinfo = $zhou_xing_gift = array();
 while($arr=$rs->FetchRow()){
-	$arr['class']=str_replace('.png','',$arr['giftimage']);
-	$arr['class']=str_replace('.gif','',$arr['class']);
-	if(in_array($arr['giftid'],$zhou_xing)){
-		$arr['week'] = true;
-		$zhou_xing_gift[$arr["giftid"]] = $arr;
-	}
-	$giftinfo[$arr['giftcateid']][]=$arr;
-	$giftId[$arr["giftid"]] = $arr;
+    $arr['class']=str_replace('.png','',$arr['giftimage']);
+    $arr['class']=str_replace('.gif','',$arr['class']);
+    if(in_array($arr['giftid'],$zhou_xing)){
+        $arr['week'] = true;
+        $zhou_xing_gift[$arr["giftid"]] = $arr;
+    }
+    $giftinfo[$arr['giftcateid']][]=$arr;
+    $giftId[$arr["giftid"]] = $arr;
 }
 shuffle($zhou_xing_gift);
 $rs=$db->Execute("select * from giftcate WHERE type=0 order by indexs DESC");
 
 $giftcate = array();
 while($arr=$rs->FetchRow()){
-	$giftcate[$arr["giftcateid"]] = $arr;
+    $giftcate[$arr["giftcateid"]] = $arr;
 }
 
 
--- trace_comic.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/10/25
- * Time: 17:45
- */ 
