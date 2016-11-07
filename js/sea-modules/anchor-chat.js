820d6c740425d95dc5f1c9df501b1048091e62fb
940613683 <940613683@qq.com>
2016-11-07 17:03:56 +0800

    1

--- js/sea-modules/anchor-chat.js
+++ js/sea-modules/anchor-chat.js
@@ -96,12 +96,11 @@ define(function(require, exports, module) {
                             var i = 3;
                             function d3() {
                                 i--;
-                                $("#sendChatBtn").text(" [" + i + "]秒");
+                                $("#sendChatNotice").text(" [" + i + "]秒");
                                 $("#sendChatBtn").attr("disabled", true);
                                 if (i == 0) {
                                     clearInterval(ti);
                                     $("#sendChatBtn").attr("disabled", false);
-                                    $("#sendChatBtn").text("发送");
                                 }
                             }
                             var ti = setInterval(d3, 1000);
