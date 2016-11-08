f558c53fadfbe906d6d45933d8799456138bffce
940613683 <940613683@qq.com>
2016-11-08 19:26:26 +0800

    1

--- js/ajax/username.js
+++ js/ajax/username.js
@@ -1,68 +1,65 @@
-define('ajax/username', function(require, exports, module){
-    exports.check = function(){
-        nickname= $("#nickname").val();
-        var data={
-            nickname:$("#nickname").val()
+define('ajax/username', function (require, exports, module) {
+    exports.check = function () {
+        nickname = $("#nickname").val();
+        var data = {
+            nickname: $("#nickname").val()
         }
         $.ajax({
             type: "POST",
             cache: false,
             url: "/ajax/refactorUserInfo.php?action=editName",
             contentType: "application/x-www-form-urlencoded",
-            data:data,
-            success:function(data){
-                data=JSON.parse(data);
-                 if(data.resultStatus == 200){
-                     //alert("修改成功!");
-                     $(".center-name").text(nickname);
-                     $(".a-myname").show();
-                     $(".c-myname").hide();
-                }else{
-                     alert(data.errorMessage);
+            data: data,
+            success: function (data) {
+                data = JSON.parse(data);
+                if (data.resultStatus == 200) {
+                    //alert("修改成功!");
+                    $(".center-name").text(nickname);
+                    $(".a-myname").show();
+                    $(".c-myname").hide();
+                } else {
+                    alert(data.errorMessage);
                 }
             }
         });
     };
-    $(".center-edit").click(function(){
+    $(".center-edit").click(function () {
         $(".a-myname").hide();
         $(".c-myname").show();
     });
-    $(".c-myname #cancel").on("click",function(){
+    $(".c-myname #cancel").on("click", function () {
 
         $(".a-myname").show();
         $(".c-myname").hide();
     })
-    $(".c-funds-dh").on("click",function(){
+    $(".c-funds-dh").on("click", function () {
         $(".zhezhao").show();
         $(".duikdou").show();
     })
-    $(".dui-cancel").on("click",function(){
+    $(".dui-cancel").on("click", function () {
         $(".zhezhao").hide();
         $(".duikdou").hide();
+        $("#duiv").val("");
     })
-    $(".dui-queren").on("click",function(){
-        var dou=$("#duiv").val() || null;
-        if(dou != null && dou !== ""){
+    $(".dui-queren").on("click", function () {
+        var dou = $("#duiv").val() || null;
+        var common=require("./common");
+        if (dou != null && dou !== "") {
             $.ajax({
                 type: "POST",
                 cache: false,
-                url: "/rest/usersGiftDetails/exchange.mt?userId=27079&xcoins=100",
-                data:JSON.stringify(""),
-                contentType:"application/json; charset=utf-8",
-                success:function(data){
-                    data=JSON.parse(data);
-                    if(data.resultStatus == 200){
-                        //alert("修改成功!");
-                        $(".center-name").text(nickname);
-                        $(".a-myname").show();
-                        $(".c-myname").hide();
+                url: "/rest/usersGiftDetails/exchange.mt?userId=" + currentUserId + "&xcoins=" + dou,
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    if (data.resultStatus == 200) {
+                        common.clert(data.resultMessage);
                     }else{
-                        alert(data.errorMessage);
+                        common.clert(data.resultMessage);
                     }
+                    $(".dui-cancel").click();
                 }
             });
         }
-        alert(dou);
 
     })
 
