30eac90b757ee8bffb22c81aaf7eb578f17437a1
daisy <fengxiaoqing128@outlook.com>
2016-12-01 15:49:55 +0800

    liveP

--- js/angular/center.js
+++ js/angular/center.js
@@ -1,11 +1,12 @@
 var app=angular.module('personalCenter',[]);
 app.controller('center',function($scope,$http){
 	$scope.news=[];
-	$http.get('',{
-		params:{act:'get',page:1}
+	$scope.replies=[{id:1,content:'试试'}]
+	/*$http.post('rest/homeAnchors/livePhone.mt?roomNumber=1044',{
+		//params:{act:'get',page:1}
 	}).success(function(arr){
-		//alert(arr)
+		alert(arr)
 	}).error(function(){
 		alert('读取失败，请稍后重试')
-	})
+	})*/
 })
