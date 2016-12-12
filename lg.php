4d4eab884d79f75fa5c9a400a478d143d6519f30
940613683 <940613683@qq.com>
2016-12-12 17:24:10 +0800

    css

--- lg.php
+++ lg.php
@@ -11,7 +11,8 @@ $str='fgz%2B28Xab%2FcbvPx1vuzMWGYZuAF1BsaBWmM1t26aaDT2%2FJ3%2BaoZFPgYsHxf0%2FWHc
 $str2 = 'CTl18IJNIEE79EssGXNIM46T1tQDX6NGLu5Qhoy2OSvD4mGkvNZub9J7nXHkZZjl';     //不知道     能解
 $str3 = "w7J%2Br9BCYQL4NSM8R9G%2B7cXAb2Gg5xIgwtbgMLfzMACjbv4cHCRfkEyo72KQHcQJ"; //不掉线     不能解
 $str4 = 'tiS1bhXrIvS9b%2FwS6OT9gBDpzT6Y1%2FFSBO1%2FEz3Rf73V873MQ8q8BYsikFgBZorm'; //不掉线         不能解
-$str5 ='Ls6RMzX2ll1S34TlbPZNRn8bBPqDQmZ%2F4ohLpx68FaBRx8W4xKwd87iy4mzNdCSF';     //
+$str5 ='Ls6RMzX2ll1S34TlbPZNRn8bBPqDQmZ%2F4ohLpx68FaBRx8W4xKwd87iy4mzNdCSF';     //不掉线         不能解
+$str6 = 'NBKhaVd2TK7QM%2BeUESUqqkfCqpMsKj7lah76xLXOMH2YZQEXD0yNoPihpYF61GO5'; //(赵海刚的) 不能解
 if(isset($_GET['str'])){
     var_dump(aes_decrypt($_GET['str']));
 }
