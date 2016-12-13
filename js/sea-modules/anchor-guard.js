e5903a9351d8c426738cf43b69c30b38de9ece86
940613683 <940613683@qq.com>
2016-12-13 20:06:53 +0800

    213

--- js/sea-modules/anchor-guard.js
+++ js/sea-modules/anchor-guard.js
@@ -12,7 +12,15 @@ define(function(require, exports, module) {
 		clz : $(".start-watch .clz"),
 		price : 88800,
 		contoiner : $(".guard-main"),
-		init : function() {
+		init : function(data) {
+            var _this = this;
+            var params = jQuery.parseJSON(data.params);
+            var rtype = UIF.roomType;
+            this.rtype = rtype;
+
+            _this.price = eval("params."+rtype+".guard")+"00";
+            $(".sfpeice").text("总价格" + _this.price * parseInt($(".sftext").text()) + "K豆");
+
 			this.sfbox = $(".start-watch");
 			this.changeTimeBtn = $(".sftext");
 			this.timeList = $(".sf-times");
@@ -95,7 +103,8 @@ define(function(require, exports, module) {
 			var base = this;
 			base.startWatchBtn.on("click", function() {
 				UIF.handler.sendAddGuard({
-					guardDT : base.changeTimeBtn.text()
+					guardDT : base.changeTimeBtn.text(),
+                    rtype : base.rtype
 				}, function(data) {
 					var args = jQuery.parseJSON(data);
 					if (args != null && args.resultStatus == 200) {//100:no money,101:nontime
