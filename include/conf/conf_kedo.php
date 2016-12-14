ff40c5f9644ac47c1e265c53c955c155734d95c7
940613683 <940613683@qq.com>
2016-12-14 11:39:14 +0800

    e

--- include/conf/conf_kedo.php
+++ include/conf/conf_kedo.php
@@ -34,4 +34,4 @@ define('_REDIS_HOST_','09ede0159aa64f40.m.cnhza.kvstore.aliyuncs.com');//redis�
 define('_REDIS_PWD_','09ede0159aa64f40:fooBa4eds');//redis地址
 define('_REDIS_KEYB_','ext');
 
-$vsn = md5(date('Y-d-m')."S%012");
+$vsn = md5(date('Y-d-m')."S%000012");
