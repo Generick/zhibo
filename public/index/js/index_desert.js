dad39cb76d91b656e27b7df0a735f35896b318b9
daisy <fengxiaoqing128@outlook.com>
2016-12-22 18:00:53 +0800

    娱乐直播增数

--- public/index/js/index_desert.js
+++ public/index/js/index_desert.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
         $(name + ' ' + title).mouseover(function () {
             $(this).addClass(cur).siblings().removeClass(cur);
             $(content + " > " + Sub).eq($(name + ' ' + title).index(this)).show().siblings().hide();
-
         })
-
-
     }
+
 
     SwapTab(".captitle", "a", ".cons", ".foLi", "active");
     SwapTab("#orderTit", "li", "#orderBody", ".orderItem", "active");
+
+
     function ndecodeURI(str){
         var $a="";
         try{
@@ -20,9 +20,10 @@ $(document).ready(function(){
         }catch (e){
             $a="错误编码";
         }
-
         return $a;
     }
+
+
     var Tools = {};
     Tools.getDate = function (params, callback) {
         $.ajax({
@@ -38,7 +39,6 @@ $(document).ready(function(){
                 if (typeof(params.error)==='function') {
                     params.error(jqXHR,textStatus,errorThrown);
                 }
-
             }
         });
     };
@@ -55,10 +55,8 @@ $(document).ready(function(){
                     if (typeof(params.error)==='function') {
                         params.error(jqXHR,textStatus,errorThrown);
                     }
-
                 },
                 200: function(){
-
                 }
             },
             success: function (data, textStatus, jqXHR) {
@@ -68,10 +66,11 @@ $(document).ready(function(){
                 if (typeof(params.error)==='function') {
                     params.error(jqXHR,textStatus,errorThrown);
                 }
-
             }
         });
     };
+
+
     Tools.stringFormat = function () {
         if (arguments.length == 0)
             return this;
@@ -86,7 +85,10 @@ $(document).ready(function(){
         }
         return $;
     };
+
+
     Tools.sliceObject=function(o,s,n){
+
         if(o==null || o==undefined || o==""){
             return false;
         }
@@ -97,10 +99,10 @@ $(document).ready(function(){
         return newo;
     }
 
+
     //关注
-
-
     $(function () {
+
         var Mycare = {};
         Mycare.listlen=0;
         Mycare.loop=0;
@@ -154,6 +156,7 @@ $(document).ready(function(){
             });
         };
 
+
         var canTop=false;
         var canBottom=false;
         // alert(Math.ceil(12/5));
@@ -206,7 +209,6 @@ $(document).ready(function(){
                 }
                 changeClose('glyphicon-menu-down-hover');
             });
-
         }
         Mycare.init();
 
@@ -215,18 +217,18 @@ $(document).ready(function(){
             data: {userId:currentUserId}
         }, function (data) {
         });
-
     })
+
 
     $(function(){
 
         var ulList={
+
             "dayList":"/files/giftWeels.json", //主播周塝
 
             "weekList":"/files/giftMonth.json", //主播月
 
             "monthList":"/files/giftYear.json", //主播年榜
-
 
             "th_week":"/files/localTyrantDay.json",//土豪周
 
@@ -234,13 +236,11 @@ $(document).ready(function(){
 
             "th_year":"/files/localTyrantYear.json",//土豪年
 
-
             "hy_week":"/files/fansActive.json",//活跃周
 
             "hy_month":"/files/fansActive.json",//活跃月
 
             "hy_year":"/files/fansActive.json",//活跃年
-
 
             "anc_banner":"/files/banners.json",//banner
 
@@ -251,9 +251,13 @@ $(document).ready(function(){
             "newHotAnchorsJson":"/files/hotAnchorsNew.json",//new hot anchors interface json
 
             "anc_game":"/files/gameAnchors.json",//精彩推荐
+
             "anc_gameType":"/rest/homeAnchors/gameType.mt",//首页游戏分类
+
             "anc_gameTypeJson":"/files/gameType.json",//首页游戏分类静态
+
             "gameHeald":"/rest/homeAnchors/gameHeald.mt",//首页游戏综合推荐
+
             "anc_new":"/files/newAnchors.json" //"/ajax/getLiveAnchors.php"+"?anc_type=new"
 
         }
@@ -266,6 +270,7 @@ $(document).ready(function(){
 					<span class="diamond fl sprite liverlevel-pic_liverlevel_{4}"></span>\
 					<a href="{5}"><span class="name fl">{6}</span> </a>\
 				</li>';
+
         var ht_ransT='<div class="row foLi"><a href="{0}" target="_blank">\
 						<span class="icon_shuzi_{1} sprite_top pull-left hidden-md hidden-sm hidden-xs"></span>\
 						<img class="pull-left img-circle lazy"  src="{2}" alt="{3}"/>\
@@ -275,6 +280,7 @@ $(document).ready(function(){
 					</div>\
 					<div class="pull-left spriteLev liverlevel-pic_liverlevel_{5}"></div>\
 					</a></div>';
+
         var ht_rans='<div class="row foLi"><a href="{0}" target="_blank">\
 						<span class="icon_shuzi_{1} sprite_top pull-left hidden-md hidden-sm hidden-xs"></span>\
 						<img class="pull-left img-circle lazy" src="{2}" alt="{3}"/>\
@@ -315,6 +321,7 @@ $(document).ready(function(){
             });
         }
 
+
         parse_to_board("dayList",ulList.dayList,"anchor");
         parse_to_board("weekList",ulList.weekList,"anchor");
         parse_to_board("monthList",ulList.monthList,"anchor");
@@ -326,7 +333,6 @@ $(document).ready(function(){
         parse_to_board("hy_week",ulList.hy_week);
         parse_to_board("hy_month",ulList.hy_month);
         parse_to_board("hy_year",ulList.hy_year);
-
 
 
         var compiliter={
@@ -350,7 +356,7 @@ $(document).ready(function(){
                     <div class="playBtn"></div></a>',
           
             bigH:
-                '<a href="{0}"  target="_blank" class="thumbnail firHot bord1d">\
+                '<a href="{0}"  target="_blank" class="thumbnail firHot bord1Pin">\
                     <div class="hotT">\
                         <img class="lazy" src="{1}&w=263&h=386" alt="{2}">\
                         {3}\
@@ -450,6 +456,7 @@ $(document).ready(function(){
                     </div>\
                     <div class="playBtn"></div>\
                 </a>',
+
             gameHtml:
                 ' <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">\
                     <a href="{0}" target="_blank" class="thumbnail padd1 bgnone">\
@@ -543,6 +550,7 @@ $(document).ready(function(){
             }
         };
 
+
         compiliter.parseAnchors = function(url,type){
             Tools.getJson({
                 url: ulList.anc_hot,
@@ -592,21 +600,18 @@ $(document).ready(function(){
                             s_sml = Tools.stringFormat(compiliter.lithtml,"col-lg-2 hidden-md hidden-sm hidden-xs",v.roomNumber,v.image, ndecodeURI(v.nickName),v.totalpoint,compiliter.tolive(v.online,1),v.numbers,ndecodeURI(v.nickName),compiliter.totime(v.onlineTime));
                         }
                         srow.append(s_sml);
-
                     })
                     swrap.append(srow);
                     $("#"+$h+" .hotLiCon").append(bwrap);
                     $("#"+$h+" .hotLiCon").append(swrap);
 
-
                     $("#hotList img.lazy").lazyload({
                         effect: "fadeIn"
                     });
-
                 }
-
             });
         }
+        
 
         //new hot anchors interface
         compiliter.parseNewHotAnchors = function(url,type){
