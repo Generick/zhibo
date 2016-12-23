0eac94c47badf8599ebe76b39cd4b75364ce3448
风中男孩 <1097700968@qq.com>
2016-12-23 13:28:11 +0800

    昨天下午更新优化的配置文件bug修复

--- app/controller/livembController.class.php
+++ app/controller/livembController.class.php
@@ -2,11 +2,11 @@
 	class livembController{
 		public function index(){
 			global $view;
-			global $vsn;
-			$page_var['cdn']=_CDNDOMAIN_;
+			//global $vsn;
+			//$page_var['cdn']=_CDNDOMAIN_;
 			
-			$roomnumber = (int)$_GET['roomnumber'];
-			
+			//$roomnumber = (int)$_GET['roomnumber'];
+			$roomnumber ='10005';
 			function curl_post_data($url,$post){
 				$options = array(
 						CURLOPT_RETURNTRANSFER=>true,
@@ -20,21 +20,32 @@
 				curl_close($ch);
 				return $result;
 			}
-			$post = array('roomnumber'=>$roomnumber);
-			$interface = _INTERFACE_."/rest/homeAnchors/livePhone.mt?roomNumber=".$roomnumber;
-			$datas = curl_post_data($interface,$post);
+			//$post = array('roomnumber'=>$roomnumber);
+			//$interface = _INTERFACE_."/rest/homeAnchors/livePhone.mt?roomNumber=".$roomnumber;
+			//$datas = curl_post_data($interface,$post);
+			$datas = curl_post(_INTERFACE_."/rest/homeAnchors/livePhone.mt?roomNumber={$roomnumber}");
 			$data = json_decode($datas,true);
+			
+			//echo (_INTERFACE_."/rest/homeAnchors/livePhone.mt?roomNumber={$roomnumber}");
 			//print_r($data);
 			$zhuboinfo = $data['data'];
 			$roomUsers = $zhuboinfo['roomUsers'];
-			//print_r($data['data']);
-			//
+			$hotAnchors = $zhuboinfo['hotAnchors'];
+			$hotNum = count($hotAnchors)<6?count($hotAnchors):6;
+			$imgsize = '&w=330&h=181';
+			//print_r($zhuboinfo);
+			//echo $hotNum;
 			$url = 'http://1b7a61-0.sh.1252349838.clb.myqcloud.com/rest/site/transcoding.mt?roomNumber='.$roomnumber;
 			$udata = array('roomnumber'=>$roomnumber);
 			$result = curl_post_data($url,$udata);
-			// var_dump($url);
-			// var_dump($result);
 			
+			// print_r($result);
+			 $view->assign('zhuboinfo',$zhuboinfo);
+			 $view->assign('hotAnchors',$hotAnchors);
+			 $view->assign('hotNum',$hotNum);
+			 $view->assign('imgsize',$imgsize);
+			 $view->assign('rmb',$roomnumber);
+			 
 			$view->display('livemoblie.html');
 		}
 		
